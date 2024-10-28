8475bfa7778c8ef9cd4229ac85c02d35ba0e044d
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-10-28 23:33:48 +0800

    treewide: adjust version counting
    offset by 6 from upstream breh

--- manager/build.gradle.kts
+++ manager/build.gradle.kts
@@ -58,7 +58,7 @@ fun getGitDescribe(): String {
 fun getVersionCode(): Int {
     val commitCount = getGitCommitCount()
     val major = 1
-    return major * 10000 + commitCount + 200
+    return major * 10000 + commitCount + 194
 }
 
 fun getVersionName(): String {
